751d730a58f818ed1ccf511bd7e09cd1c461b1c6
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-04-17 14:16:09 +0300

    Dont display message if it's a reaction
    Don't display message if it's a reaction
    
    Revert old changes
    
    Revert "Don't display message if it's a reaction"
    
    This reverts commit 54af1b4652f640ab09067e90c284c2b6c4f1b76c.
    
    Don't display message if it's a reaction
    
    Add flag to enable/disable reactions in chat

--- react/features/chat/components/AbstractMessageContainer.ts
+++ react/features/chat/components/AbstractMessageContainer.ts
@@ -1,40 +1,37 @@
 import { Component } from 'react';
+import { ReactReduxContext } from 'react-redux';
 
+import { isReactionsInChatEnabled } from '../functions';
 import { IMessage } from '../types';
 
 export interface IProps {
-
-    /**
-     * The messages array to render.
-     */
     messages: IMessage[];
 }
 
-/**
- * Abstract component to display a list of chat messages, grouped by sender.
- *
- * @augments PureComponent
- */
 export default class AbstractMessageContainer<P extends IProps, S> extends Component<P, S> {
+    static contextType = ReactReduxContext;
+    declare context: React.ContextType<typeof ReactReduxContext>;
+
     static defaultProps = {
         messages: [] as IMessage[]
     };
 
-    /**
-     * Iterates over all the messages and creates nested arrays which hold
-     * consecutive messages sent by the same participant.
-     *
-     * @private
-     * @returns {Array<Array<Object>>}
-     */
     _getMessagesGroupedBySender() {
         const messagesCount = this.props.messages.length;
         const groups: IMessage[][] = [];
         let currentGrouping: IMessage[] = [];
         let currentGroupParticipantId;
 
+        const { store } = this.context;
+        const state = store.getState();
+        const reactionsInChatEnabled = isReactionsInChatEnabled(state);
+
         for (let i = 0; i < messagesCount; i++) {
             const message = this.props.messages[i];
+
+            if (message.isReaction && !reactionsInChatEnabled) {
+                continue;
+            }
 
             if (message.participantId === currentGroupParticipantId) {
                 currentGrouping.push(message);
