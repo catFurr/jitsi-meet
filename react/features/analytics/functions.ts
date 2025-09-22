5ef5f9afe00fbac8ccd65f0b572fe60afbf3cfed
ibrahim ali <41322490+catFurr@users.noreply.github.com>
2025-09-22 22:28:42 +0300

    removed amplitude

--- react/features/analytics/functions.ts
+++ react/features/analytics/functions.ts
@@ -19,6 +19,7 @@ import { parseURLParams } from '../base/util/parseURLParams';
 import { parseURIString } from '../base/util/uri';
 import { isPrejoinPageVisible } from '../prejoin/functions';
 
+import AmplitudeHandler from './handlers/AmplitudeHandler';
 import MatomoHandler from './handlers/MatomoHandler';
 import logger from './logger';
 
@@ -108,7 +109,6 @@ export async function createHandlers({ getState }: IStore) {
 
     if (amplitudeAPPKey) {
         try {
-            const { default: AmplitudeHandler } = await import('./handlers/AmplitudeHandler');
             const amplitude = new AmplitudeHandler(handlerConstructorOptions);
 
             analytics.amplitudeIdentityProps = amplitude.getIdentityProps();
