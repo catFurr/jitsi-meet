d058d8223c499e363b2010f104ec20e00f37a2fe
Zaid0412 <108808139+Zaid0412@users.noreply.github.com>
2025-09-25 10:31:30 +0300

    fix redirect path

--- react/features/conference/functions.web.ts
+++ react/features/conference/functions.web.ts
@@ -23,7 +23,7 @@ export function maybeShowSuboptimalExperienceNotification(dispatch: IStore['disp
                         t,
                         'notify.suboptimalBrowserWarning',
                         {
-                            recommendedBrowserPageLink: `${window.location.origin}/static/recommendedBrowsers.html`
+                            recommendedBrowserPageLink: `${window.location.origin}/static/recommendedBrowsers`
                         }
                     )
                 }, NOTIFICATION_TIMEOUT_TYPE.LONG
